cd715a60a37684df4df2601009108f3aa7ec7605
IceCream <www@withdata.net>
2020-06-09 17:04:10 +0800

    将 YuQ-API 分离出项目。

--- src/main/kotlin/com/icecreamqaq/yuq/error/err.kt
+++
@@ -1,6 +0,0 @@
-package com.icecreamqaq.yuq.error
-
-open class YuQException(message: String? = null, cause: Throwable? = null) : Exception(message, cause)
-open class YuQRuntimeException(message: String? = null, cause: Throwable? = null) : RuntimeException(message, cause)
-
-class ImageTypedException(message: String) : YuQRuntimeException(message = message)
