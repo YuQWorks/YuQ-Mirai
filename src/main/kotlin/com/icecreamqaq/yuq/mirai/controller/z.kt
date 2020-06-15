a912052d7555703ed5f609baaaa92d8c1fbe9a87
IceCream <www@withdata.net>
2020-06-15 22:20:15 +0800

    fix

---
+++ src/main/kotlin/com/icecreamqaq/yuq/mirai/controller/z.kt
@@ -0,0 +1,10 @@
+package com.icecreamqaq.yuq.mirai.controller
+
+import com.icecreamqaq.yuq.annotation.GroupController
+import com.icecreamqaq.yuq.annotation.PrivateController
+
+@PrivateController
+class ZPC
+
+@GroupController
+class ZGC
